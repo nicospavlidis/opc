b6c0766e4170cbdaf81ee39c5c1021e6e42a547b
nicospavlidis <npvlds@gmail.com>
2018-06-14 12:46:48 +0100

    Fixed case where two clusters share the same centroid

--- src/generic/bisKmeansPP.m
+++ src/generic/bisKmeansPP.m
@@ -1,7 +1,12 @@
 function [v,fval,idx] = bisKmeansPP(X)
 	[idx,C,sumd] = kmeans(X,2,'EmptyAction','singleton','Replicates',1);
 	v = (C(1,:) - C(2,:))';
-	v = v./norm(v,2);;
+	if sum(abs(v))<eps,
+		v = pcacomp(X,1);
+		fval = -inf;
+		return;
+	end
+	v = v./norm(v,2);
 
 	% Recommended in Steinbach, Karypis and Kumar. A Comparison of Document Clustering Techniques. 2000
 	fval = size(X,1);
