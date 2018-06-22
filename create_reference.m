eb40e034baf4a06d0f99a7914fcb878fc51168fd
nicospavlidis <npvlds@gmail.com>
2018-06-22 10:59:54 +0100

    include ignoreDir

--- create_reference.m
+++ create_reference.m
@@ -1,1 +1,1 @@
-m2html('mfiles','src','htmldir','documentation/reference','recursive','on','global','on','template','docs','source','off')
+m2html('mfiles','src','htmldir','documentation/reference','recursive','on','global','on','template','docs','source','off','ignoredDir','figtree-0.9.3')
