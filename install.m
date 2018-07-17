5b8f30bde1de74cd0e194e0ffa8d76b2879b1241
nicospavlidis <npvlds@gmail.com>
2018-07-17 12:09:58 +0100

    install updates

--- install.m
+++ install.m
@@ -1,6 +1,8 @@
 cd src/mddcFcns/
 mexme
 cd ../../
+% This needs to be modified accordingly if the FigTree library
+% is stored in a different location
 cd src/libs/figtree-0.9.3/matlab/
 CompileMexFilesUsingMatlab
 cd ../../../../
