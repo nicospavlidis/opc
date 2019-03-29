d5af6f1375a43504f03dfed75144d27960dd303e
nicospavlidis <npvlds@gmail.com>
2019-03-29 11:45:16 +0000

    Added functionality for data whitening

--- src/generic/normalise.m
+++ src/generic/normalise.m
@@ -1,20 +1,23 @@
-function Data = normalise(Data, standard);
+function [Data,S] = normalise(Data, standard);
 %Removes observations (rows) with missing values and variables (columns) with no variation
-%DATA = NORMALISE(DATA, STANDARD);
+%DATA = NORMALISE(X, STANDARD);
 % 
 % Inputs:
-%	(DATA): N-by-D data matrix (observations stored row-wise)
-%	(STANDARD): If (STANDARD)=1, then variables are standardised. Default = 0
+%	(X): N-by-D data matrix (observations stored row-wise)
+%	(STANDARD): If (STANDARD)=1, then variables/ columns are standardised. 
+%		If (STANDARD)=='whiten' the whitening transformation is performed.
+%		default = 0
 %
 % Output:
-%	(DATA): N-by-D data matrix
+%	(X): N-by-D data matrix
+%	(S):	In (STANDARD)=='whiten' (S) is the Cholesky decomposition of (X)
 
 %-------------------------------------------------------------------------------------
 % Copyright @ Nicos Pavlidis, 2018
 % OPC is licensed under the BSD-3-Clause License - see the LICENSE.md file for details
 %-------------------------------------------------------------------------------------
 
-if nargin < 2,
+if nargin < 2 || isempty(standard),
 	standard=0;
 end
 
@@ -25,7 +28,11 @@ if sum(inactive) > 0,
 end
 
 Data = bsxfun(@minus, Data, mean(Data));
-if standard,
+if strcmp(standard,'whiten'),
+	% Cholesky decomposition of data matrix
+	S = chol(cov(Data));
+	Data = Data*pinv(S);
+elseif standard,
 	Data = bsxfun(@rdivide, Data, std(Data)); 
 end
 
